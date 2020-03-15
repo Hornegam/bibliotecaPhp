f8dfaaf4dcbdda607b0f5545327898a725ff8ff0
DevFigueiredo <miranda.f@aluno.ifsp.edu.br>
2020-03-15 12:42:23 -0300

    Criado os arquivos StatusModel e SugerirModel

---
+++ model/SugerirModel.php
@@ -0,0 +1,30 @@
+<?php
+
+include_once 'Conexao.php';
+
+class SugerirModel {
+
+    private $prontuarioUser;
+    private $codLivro;
+    private $codEmpr;
+    private $dataRet;
+    private $dataDev;
+
+    public function criar() {
+    }
+
+    public function listar(){
+    }
+
+    public function update($campo, $valor, $id){
+    }
+
+    public function delete($id){
+    }
+
+
+function __construct() {
+    $this->conexao = Conexao::getInstance();
+}
+
+}
