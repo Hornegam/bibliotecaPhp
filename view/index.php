2ec695bc922cb1247ae4955601b41278e38ade1f
DevFigueiredo <miranda.f@aluno.ifsp.edu.br>
2020-03-15 21:54:24 -0300

    oi

--- view/index.php
+++ view/index.php
@@ -9,20 +9,24 @@
        // include_once '../model/AgendarModel.php';
        // include_once '../model/comentarModel.php';
        // include_once '../model/EmprestarModel.php';
-       // include_once '../model/LivroModel.php';
-       // include_once '../model/StatusModel.php';
+        include_once '../model/LivroModel.php';
+        include_once '../model/StatusModel.php';
        // include_once '../model/SugerirModel.php';
        // include_once '../model/UsuarioModel.php';
        
        //$status = new StatusModel();
        //$status->criar(4,"Teste");
-       //$status->listar();
        //$status->update("tipoStatus", "Disponível", 0);
+     $livro = new LivroModel();
+     $livros = $livro->listar();
+    // echo $livro->getTitulo();
+     
+
        
        //$livro = new LivroModel();
        //var_dump($livro->listarLivro(1));
        
-       $sugerir = new SugerirModel();
+      // $sugerir = new SugerirModel();
 
 
        ?>
